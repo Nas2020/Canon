ebf9a2a95060d0d6ad8c65c2ebfe910937f4a6c5
nas2020 <naseer8518@gmail.com>
2023-08-12 16:31:42 -0700

    Updated

--- ingestion-service/src/blockchain/events.ts
+++ ingestion-service/src/blockchain/events.ts
@@ -1,146 +1,124 @@
 import Web3 from 'web3';
 import mongoose from 'mongoose';
+import { processInteractionData } from './process-interaction-data';
+
+async function fetchDataFromMongoBlock() {
+    const collections = await mongoose.connection.db.listCollections({ name: 'recentblocknumber' }).toArray();
+
+    if (collections.length > 0) {
+        // If the collection exists, fetch data
+        const dataFromMongoBlock = await mongoose.connection.collection('recentblocknumber').find({}).toArray();
+        console.log("dataFromDB", dataFromMongoBlock);
+        return dataFromMongoBlock;
+    } else {
+        console.log("'mongoBlockData' collection does not exist.");
+        return null
+    }
+}
 
 export async function subscribeToNewBlocks(web3: Web3, contractAddress: string, abi: any) {
-    //console.log("ABI:", abi);
 
 
     // Initialize contract object with web3
-    const utilityContract = new web3.eth.Contract(abi, contractAddress);
+    //const utilityContract = new web3.eth.Contract(abi, contractAddress);
+    // // Event listener for NYMRegistered
+    // const nymREvents = await utilityContract.events.NYMRegistered({ fromBlock: 'latest' })
+    // nymREvents.on('data', event => {
+    //     console.log('NYMRegistered event received:', event);
 
+    //     // Extract event details 
+    //     const eventData = {
+    //         role: event.returnValues.role,
+    //         version: event.returnValues.version,
+    //         endpoint: event.returnValues.endpoint
+    //     };
+    //     console.log('eventData', eventData);
+    //     // Save event details to MongoDB
+    //     mongoose.connection.collection('NYMRegisteredEvents').insertOne(eventData);
+    // })
+    // // Event listener for SchemaRegistered
+    // const SchemaREvents = await utilityContract.events.SchemaRegistered({ fromBlock: 'latest' })
+    // SchemaREvents.on('data', event => {
+    //     console.log('SchemaRegistered event received:', event);
 
-    // Event listener for NYMRegistered
-    const nymREvents = await utilityContract.events.NYMRegistered({ fromBlock: 'latest' })
-    nymREvents.on('data', event => {
-        console.log('NYMRegistered event received:', event);
+    //     // Extract event details 
+    //     const eventData = {
+    //         schemaID: event.returnValues.schemaID,
+    //         name: event.returnValues.name
+    //         // ... capture other relevant details as needed
+    //     };
+    //     console.log('eventData', eventData);
+    //     // Save event details to MongoDB
+    //     mongoose.connection.collection('SchemaRegisteredEvents').insertOne(eventData);
+    // })
+    // // Event listener for CredDefRegistered
+    // const CredREvents = await utilityContract.events.CredDefRegistered({ fromBlock: 'latest' })
+    // CredREvents.on('data', event => {
+    //     console.log('CredDefRegistered event received:', event);
 
-        // Extract event details 
-        const eventData = {
-            role: event.returnValues.role,
-            version: event.returnValues.version,
-            endpoint: event.returnValues.endpoint
-        };
-        console.log('eventData', eventData);
-        // Save event details to MongoDB
-        mongoose.connection.collection('NYMRegisteredEvents').insertOne(eventData);
-    })
-    // Event listener for SchemaRegistered
-    const SchemaREvents = await utilityContract.events.SchemaRegistered({ fromBlock: 'latest' })
-    SchemaREvents.on('data', event => {
-        console.log('SchemaRegistered event received:', event);
+    //     // Extract event details 
+    //     const eventData = {
+    //         credDefID: event.returnValues.credDefID,
+    //         tag: event.returnValues.tag
+    //     };
+    //     console.log('eventData', eventData);
+    //     // Save event details to MongoDB
+    //     mongoose.connection.collection('CredRegisteredEvents').insertOne(eventData);
+    // })
 
-        // Extract event details 
-        const eventData = {
-            schemaID: event.returnValues.schemaID,
-            name: event.returnValues.name
-            // ... capture other relevant details as needed
-        };
-        console.log('eventData', eventData);
-        // Save event details to MongoDB
-        mongoose.connection.collection('SchemaRegisteredEvents').insertOne(eventData);
-    })
-    // Event listener for CredDefRegistered
-    const CredREvents = await utilityContract.events.CredDefRegistered({ fromBlock: 'latest' })
-    CredREvents.on('data', event => {
-        console.log('CredDefRegistered event received:', event);
-
-        // Extract event details 
-        const eventData = {
-            credDefID: event.returnValues.credDefID,
-            tag: event.returnValues.tag
-        };
-        console.log('eventData', eventData);
-        // Save event details to MongoDB
-        mongoose.connection.collection('CredRegisteredEvents').insertOne(eventData);
-    })
-
-
-
-    console.log("Test POINT 2")
+    const response = await fetchDataFromMongoBlock();
     const subscription = await web3.eth.subscribe('newHeads');
+
     subscription.on('data', async blockhead => {
-        console.log('New block header: ', blockhead);
         const transactionCount = await web3.eth.getBlockTransactionCount();
-        console.log("transactionCount", transactionCount)
+
         const block = await web3.eth.getBlock(blockhead.number, true);
+
+
+
         if (transactionCount > 0) {
 
-            const blockData = {
-                blockNumber: block.number,
-                blockHash: block.hash,
-                // include other block details you need
-                transactions: block.transactions.map((tx: any) => ({
-                    txHash: tx.hash
-                    // include other transaction details you need
-                }))
 
-            };
-
-            // Detecting transaction type
             block.transactions.forEach(async (transaction: any) => {
+
                 const txDetails = await web3.eth.getTransaction(transaction.hash);
                 const receipt = await web3.eth.getTransactionReceipt(transaction.hash);
-                console.log("receipt", receipt)
+                // console.log("receipt", receipt)
                 if ((txDetails.to === null || txDetails.to === undefined) && (txDetails.input !== null && txDetails.input !== undefined)) {
 
                     const contractData = {
                         contractAddress: receipt.contractAddress,
-                        // include other contract details you need
+                        transactionHash: transaction.hash
                     };
 
                     // save contract details to MongoDB
                     mongoose.connection.collection('contracts').insertOne(contractData);
 
-                    console.log(`Transaction ${transaction.hash} is a contract deployment with contract address: ${receipt.contractAddress}`);
-                } else if ((txDetails.to !== null && txDetails.to !== undefined) && (txDetails.input !== null && txDetails.input !== undefined)) {
-                    const interactionData = {
-                        transactionHash: transaction.hash,
-                        // include other contract interaction details you need
-                    };
+                    console.log(`Transaction ${transaction.hash} is a contract deployment with contract address: ${receipt.contractAddress}--- `);
 
-
-                    console.log("receipt.logs.topics", receipt.logs[0].topics)
-                    if ((receipt.logs[0].topics?.length as any) > 1)
-                        console.log("id", receipt.logs[0].topics![1].slice(0, 42))
-
-
-                    const data = receipt.logs[0].data
-                    console.log("receipt.logs.data", data)
-                    const offset = parseInt((<string>data).slice(0, 66), 16) * 2;
-
-
-                    // Read the length of the string (usually 32 bytes)
-                    const length = parseInt((<string>data).slice(offset, offset + 64), 16) * 2;
-
-                    // Extract the string data
-                    const rawStringData = (<string>data).slice(offset + 64, offset + 64 + length);
-
-                    // Convert the hex data to a string
-                    const resultString = web3.utils.hexToUtf8('0x' + rawStringData);
-
-                    console.log("resultString", resultString);
-
-
-
-                    // save contract interaction details to MongoDB
-                    mongoose.connection.collection('interactions').insertOne(interactionData);
-
-                    console.log(`Transaction ${transaction.hash} is a contract interaction`);
                 }
-                //  else if ((txDetails.to !== null && txDetails.to !== undefined) && (txDetails.input === null || txDetails.input === undefined || txDetails.input === '0x')) {
 
-                //     console.log(`Transaction ${transaction.hash} is an Ether transfer`);
-                // }
+                else if ((txDetails.to !== null && txDetails.to !== undefined) && (txDetails.input !== null && txDetails.input !== undefined)) {
+
+                    processInteractionData(receipt, web3);
+          
+                }
             });
             // save block and transaction details to MongoDB
-            mongoose.connection.collection('blocks').insertOne(blockData);
-
-
-
+            //mongoose.connection.collection('blocks').insertOne(blockData);
 
         } else {
             console.log("Block does not contain any transactions.");
         }
+
+        const blockData = {
+            recentBlockNumber: block.number,
+        };
+
+        // Save event details to MongoDB
+        const blocknumberCollection = mongoose.connection.collection('recentblocknumber');
+        blocknumberCollection.replaceOne({}, blockData, { upsert: true });
+
 
     });
 
--- ingestion-service/src/index.ts
+++ ingestion-service/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
+import mongoose from 'mongoose';
 import { connectToDatabase } from './config/mongoose';
 import getWeb3Instance from './config/web3';
 import { subscribeToNewBlocks } from './blockchain/events';
-import { getPeerCount, getPeers } from './blockchain/network';
+import { getPeers } from './blockchain/network';
 import { subscribeToNewPendingTransactions } from './blockchain/transactionEvents';
 import dotenv from 'dotenv';
 
@@ -12,8 +13,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const contractAddress = process.env.CONTRACTADDRESS || "0x13274fe19c0178208bcbee397af8167a7be27f6f";
-//const abi = process.env.ABI || ""
+const contractAddress = process.env.CONTRACTADDRESS || "";
+
+//const contractdAddressPath = path.join(__dirname, '../../compile-deploy-typescript', 'UtilityContractAddress.bin');
+// console.log("contractdAddressPath", contractdAddressPath)
+// const contractAddress = fs.readFileSync(contractdAddressPath, 'utf8') || process.env.CONTRACTADDRESS as string;
 
 const abiPath = path.join(__dirname, '../../compile-deploy-typescript/', 'UtilityAbi.json');
 const abi = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
@@ -21,26 +25,43 @@ const abi = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
 // Connect to MongoDB
 connectToDatabase();
 
+
+function extractDesiredFields(peer: any) {
+    const { version, name, network, port, id, protocols, enode } = peer;
+    return { version, name, network, port, id, protocols, enode };
+}
+
+
 // Connect to the blockchain
 (async () => {
     const web3 = await getWeb3Instance();
     if (web3) {
-        console.log('******************************CONNECTED TO CANON TESTNET******************************');
-        subscribeToNewBlocks(web3, contractAddress, abi);
-        //subscribeToNewPendingTransactions(web3);  // Call the function here
-        const peerCount = await getPeerCount(web3);
-        console.log(`Connected to ${peerCount} peers`);
-        const peerInfo = await getPeers(web3);
-       // console.log(`Peers metadata ${JSON.stringify(peerInfo, null, 2)}`);
 
-        //Getting current nodeinformation
-        // const block = await web3.eth.getBlock(); //Latest block
-        // console.log("block", block)
-        // const clientVersion = await web3.eth.getNodeInfo();
-        // console.log('Client version:', clientVersion);
-        await web3.eth.getAccounts().then(console.log);
-        const status = await web3.currentProvider?.getStatus();
-        console.log('status:', status);
+        subscribeToNewBlocks(web3, contractAddress, abi);
+        //subscribeToNewPendingTransactions(web3); 
+
+        // const peerCount = await getPeerCount(web3);
+        // console.log(`Connected to ${peerCount} peers`);
+        const peerInfo = await getPeers(web3);
+
+        // Transform the peerInfo data
+        let transformedPeerInfo;
+        if (peerInfo)
+            transformedPeerInfo = peerInfo.map(extractDesiredFields);
+        const networkData = {
+            peerInfo: transformedPeerInfo
+        };
+        //console.log('networkData', networkData);
+
+        // Save event details to MongoDB
+        const networkCollection = mongoose.connection.collection('peers');
+        networkCollection.replaceOne({}, networkData, { upsert: true });
+
+        //mongoose.connection.collection('network').insertOne(networkData);
+        // console.log(`Peers metadata ${JSON.stringify(peerInfo, null, 2)}`);
+        // await web3.eth.getAccounts().then(console.log);
+        // const status = await web3.currentProvider?.getStatus();
+        // console.log('status:', status);
 
 
     } else {
